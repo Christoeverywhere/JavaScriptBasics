08af87d6d81ec34f658bb45759093ea54a426112
Christopher Justin <christopherwilsonjust@gmail.com>
2025-09-07 19:21:26 +0530

    Create First.js

---
+++ First.js
@@ -0,0 +1,10 @@
+//ALL FOLLOWING TO BE TYPED IN CONSOLE
+//to show popups in the menu
+alert('Hello World')
+//to remove the entire middle part of a website for given text
+document.body.innerHTML='Hello World"
+2+2
+2-2
+2/2
+2*2
+//order of operation * / + 
